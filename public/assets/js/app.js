8c2eef415bda642ff8d777037c1b613b5fa03df2
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-05-18 00:01:32 -0700

    html and css started

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,45 @@
-console.log("ping");
+const addBurger = _ => {
+    fetch('/burgers'), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'applicaiton/json'
+        },
+        body: JSON.stringify({
+            burger_name: document.querySelector('#burgerType').value,
+            isDevoured: false
+        })
+    }
+    .then (_ => {
+        // reload the page
+        location.reload()
+    })
+    .catch(e => console.log(e))
+}
+
+const devourBurger = id => {
+    fetch(`/burgers/${target.dataset.uid}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    })
+    .then (_ => {
+        // reload the page
+        location.reload()
+    })
+    .catch(e => console.log(e))
+}
+
+document.addEventListener('click', ({target}) => {
+    switch(target.id) {
+        case 'submit':
+            console.log('hello')
+            addBurger()
+        break
+        case 'devourBtn':
+            console.log('id')
+            // devourBurger(target.dataset.uid)
+        break
+    }
+})
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 const db = require('../config')
 
 module.exports = app => {
-  // your routes here...
-  app.get('/', (req, res) => res.render('index'))
+  app.get('/', (req, res) => {
+    db.query('SELECT * FROM burgers WHERE isDevoured=false', (e, burgers) => {
+      if (e) throw e
+      res.render('index', {burgers})
+    })
+  })
+
+  app.post('/users'), (req, res) => {
+    db.query('INSERT INTO burgers SET ?', req.body, (e) => {
+      if (e) throw e
+      res.sendStatus(200)
+    })
+  }
+
+  app.put('/users/:id'), (req, res) => {
+    db.query(`UPDATE burgers SET isDevoured to false WHERE id=${id}`, (e) => {
+      if (e) throw e
+      res.render('index', {burgers})
+    })
+  }
 }
