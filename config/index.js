c16d8b911bc45e4bb09b17b147d4b8877b50e572
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-05-20 22:54:16 -0700

    added jaws db connection info

--- config/index.js
+++ config/index.js
@@ -1,9 +1,10 @@
 const { createConnection } = require('mysql2')
 
 module.exports = createConnection({
-  host: 'localhost',
+  host: 'm7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   port: null,
-  user: 'root',
-  password: 'root1234',
-  database: 'burgers_db'
+  user: 'ia6tpfx29s67o9jk',
+  password: 'yy9b4gydyltspv28',
+  database: 'mimu56yfo1gm5mmw'
 })
+// mysql://ia6tpfx29s67o9jk:yy9b4gydyltspv28@m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mimu56yfo1gm5mmw
