de7eed00ac2405290c057cf94203915e5e38a504
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-05-20 23:52:34 -0700

    fixed spacing issue updated read me

--- burgers_schema.sql
+++ burgers_schema.sql
@@ -1,13 +1,18 @@
 -- DROP  DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
-USE burgers_db;
+USE burgers;
+DELETE FROM burgers_db
+WHERE burger_name = 'Avocado Burger';
+
 
 CREATE TABLE burgers (
 	id INT(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	burger_name VARCHAR(200) NOT NULL,
     isDevoured BOOLEAN DEFAULT false
 );
+
+
 
 -- UPDATE burgers
 -- SET isDevoured = true
