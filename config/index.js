ff9425f2e47820f478088a553fcfdab665ebc37d
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-05-20 22:57:18 -0700

    changed back to localhost login info

--- config/index.js
+++ config/index.js
@@ -1,10 +1,9 @@
 const { createConnection } = require('mysql2')
 
 module.exports = createConnection({
-  host: 'm7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  host: 'localhost',
   port: null,
-  user: 'ia6tpfx29s67o9jk',
-  password: 'yy9b4gydyltspv28',
-  database: 'mimu56yfo1gm5mmw'
+  user: 'root',
+  password: 'root1234',
+  database: 'burgers_db'
 })
-// mysql://ia6tpfx29s67o9jk:yy9b4gydyltspv28@m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/mimu56yfo1gm5mmw
